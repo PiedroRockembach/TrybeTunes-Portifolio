6f4de344f76b9320c211114464651f9a0a060828
Piedro Rockembach Nunes <piedrorn@gmail.com>
2022-12-13 17:48:53 -0300

    add a login sistem and redirect the user to the search page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -8,12 +8,66 @@ import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
 
+import { createUser } from './services/userAPI';
+
 class App extends React.Component {
+  state = {
+    userName: '',
+    validName: false,
+    loading: false,
+    loged: false,
+  };
+
+  sendUser = () => {
+    this.setState({ loading: true }, async () => {
+      const { userName } = this.state;
+      await createUser({ name: userName });
+      this.setState({ loading: false, loged: true });
+    });
+  };
+
+  nameValidate = () => {
+    const { userName } = this.state;
+    const validNumber = 3;
+    if (userName.length >= validNumber) this.setState({ validName: true });
+  };
+
+  inputChange = ({ target }) => {
+    const state = target.name;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    console.log(state);
+    console.log(value);
+    this.setState({
+      [state]: value,
+    }, () => {
+      this.nameValidate();
+    });
+  };
+
   render() {
+    const {
+      userName,
+      validName,
+      loading,
+      loged,
+    } = this.state;
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" render={ () => <Login /> } />
+          <Route
+            exact
+            path="/"
+          >
+            { loged ? <Redirect to="/search" />
+              : (
+                <Login
+                  userName={ userName }
+                  inputChange={ this.inputChange }
+                  validName={ validName }
+                  createUser={ this.sendUser }
+                  loading={ loading }
+                />)}
+          </Route>
           <Route exact path="/search" render={ () => <Search /> } />
           <Route exact path="/album/:id" render={ () => <Album /> } />
           <Route exact path="/favorites" render={ () => <Favorites /> } />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Loading from './Loading';
 
 class Login extends Component {
   render() {
+    const {
+      userName,
+      inputChange,
+      validName,
+      createUser,
+      loading,
+    } = this.props;
     return (
-      <div data-testid="page-login">Login</div>
+      loading ? <Loading />
+        : (
+          <div data-testid="page-login">
+            <input
+              data-testid="login-name-input"
+              type="text"
+              value={ userName }
+              name="userName"
+              onChange={ inputChange }
+            />
+            <button
+              type="submit"
+              data-testid="login-submit-button"
+              disabled={ !validName }
+              onClick={ createUser }
+            >
+              Entrar
+            </button>
+          </div>
+        )
     );
   }
 }
 
+Login.propTypes = {
+  userName: PropTypes.string,
+  inputChange: PropTypes.func,
+}.isRequired;
 export default Login;
