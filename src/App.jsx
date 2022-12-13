9e18a08efc96bba5ce18dcf5dda38bd68e468d6e
Piedro Rockembach Nunes <piedrorn@gmail.com>
2022-12-13 16:05:41 -0300

    add Switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -12,13 +12,15 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" render={ () => <Login /> } />
-        <Route exact path="/search" render={ () => <Search /> } />
-        <Route exact path="/album/:id" render={ () => <Album /> } />
-        <Route exact path="/favorites" render={ () => <Favorites /> } />
-        <Route exact path="/profile" render={ () => <Profile /> } />
-        <Route exact path="/profile/edit" render={ () => <ProfileEdit /> } />
-        <Route exact path="*" render={ () => <NotFound /> } />
+        <Switch>
+          <Route exact path="/" render={ () => <Login /> } />
+          <Route exact path="/search" render={ () => <Search /> } />
+          <Route exact path="/album/:id" render={ () => <Album /> } />
+          <Route exact path="/favorites" render={ () => <Favorites /> } />
+          <Route exact path="/profile" render={ () => <Profile /> } />
+          <Route exact path="/profile/edit" render={ () => <ProfileEdit /> } />
+          <Route path="*" render={ () => <NotFound /> } />
+        </Switch>
       </BrowserRouter>
     );
   }
