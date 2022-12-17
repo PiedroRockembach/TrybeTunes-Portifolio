abddf4bb93be0a5ac857e9efc86b48036622d49c
Piedro Rockembach Nunes <piedrorn@gmail.com>
2022-12-17 20:31:30 -0300

    07. show music list when an album has been choosed

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MusicCard extends Component {
+  render() {
+    const {
+      url,
+      trackName,
+    } = this.props;
+    return (
+      <div>
+        <h1>{trackName}</h1>
+        <audio data-testid="audio-component" src={ url } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          {' '}
+          <code>audio</code>
+          .
+        </audio>
+      </div>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  url: PropTypes.string,
+  trackName: PropTypes.string,
+}.isRequired;
+export default MusicCard;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,14 +1,69 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import Loading from './Loading';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Album extends Component {
+  state = {
+    loading: true,
+    musicsList: [],
+    artist: '',
+
+  };
+
+  componentDidMount() {
+    const prop = this.props;
+    const { id } = prop.match.params;
+    this.getData(id).then((data) => {
+      this.setState({
+        musicsList: data,
+        artist: data[0].artistName,
+        loading: false,
+      });
+    });
+  }
+
+  getData = async (id) => {
+    const albumInfo = await getMusics(id);
+    // console.log(albumInfo);
+    return albumInfo;
+  };
+
   render() {
+    const {
+      loading,
+      artist,
+      musicsList,
+    } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        {loading ? <Loading /> : (
+          <section>
+            <h1 data-testid="artist-name">{artist}</h1>
+            <h2 data-testid="album-name">
+              {`${musicsList[0].collectionName}`}
+            </h2>
+            <ul>
+              {musicsList.map((music, index) => (
+                index !== 0 && (
+                  <MusicCard
+                    key={ music.trackName }
+                    trackName={ music.trackName }
+                    url={ music.previewUrl }
+                  />
+                )))}
+            </ul>
+          </section>
+        )}
       </div>
     );
   }
 }
 
+Album.propTypes = {
+  id: PropTypes.string,
+}.isRequired;
 export default Album;
