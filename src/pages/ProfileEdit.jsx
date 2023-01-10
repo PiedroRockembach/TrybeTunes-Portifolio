3cb238dd5d7d53dc2505bccde9b94d0937a2d147
Piedro Rockembach Nunes <piedrorn@gmail.com>
2023-01-10 00:45:35 -0300

    add apologies

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 class ProfileEdit extends Component {
   render() {
     return (
-      <div data-testid="page-profile-edit">
-        oi
+      <div data-testid="page-profile-edit" className="profile-edit">
+        <h1>Recurso ainda não disponível</h1>
+        <p>Lamento</p>
+        <p>
+          Este site está sendo feito como forma de estudo e este recurso ainda não
+          foi finalizado.
+        </p>
+        <p>Em breve o site estará completo.</p>
       </div>
     );
   }
